194861a6e4c25cbfb3403ff86938160537647d5f
ryhhill1998 <ryhhill1998@outlook.com>
2022-09-10 17:34:28 +0100

    improved layout and UI

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { useState, useEffect } from "react";
 import "./App.scss";
 
 const App = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+  // const [searchQuery, setSearchQuery] = useState("");
   const [countryArea, setCountryArea] = useState([]);
   const [countryCapital, setCountryCapital] = useState([]);
   const [countryFlag, setCountryFlag] = useState([]);
   const [countryPopulation, setCountryPopulation] = useState([]);
 
   const url = "https://restcountries.com/v3.1/name";
-  const query = "france";
+  const url2 = "https://countryflagsapi.com/png";
+  const query = "spain";
+
+  const backgroundImg = `${process.env.PUBLIC_URL}/images/image.png`;
 
   useEffect(() => {
     getCountryData(query);
+    getCountryFlag(query);
   }, []);
 
   const getCountryData = async (country) => {
@@ -20,26 +24,40 @@ const App = () => {
     const response = await fetch(`${url}/${country}`);
     const [data] = await response.json();
     console.log(data);
-    const { area, capital, flag, population } = data;
+    const { area, capital, population } = data;
     setCountryArea(area);
     setCountryCapital(capital);
-    setCountryFlag(flag);
     setCountryPopulation(population);
   };
 
+  const getCountryFlag = async (country) => {
+    const response = await fetch(`${url2}/${country}`);
+    setCountryFlag(response.url);
+  };
+
   return (
-    <div className="container">
-      <h1 className="title">Country Database</h1>
+    <div
+      className="container"
+      style={{ backgroundImage: `url(${backgroundImg})` }}
+    >
+      <div className="title-container">
+        <h1 className="title">Country Database</h1>
+      </div>
       <input
         className="search-countries"
         type="search"
         placeholder="Search countries"
       ></input>
-      <div className="country-data">
-        <h2>{countryArea}</h2>
-        <h2>{countryCapital}</h2>
-        <h2>{countryFlag}</h2>
-        <h2>{countryPopulation}</h2>
+      <div className="country-container">
+        <div className="flag-img-container">
+          <img className="flag-img" src={countryFlag} alt={`${query} flag`} />
+        </div>
+        <div className="country-details-container">
+          <h2>Country: {query}</h2>
+          <h2>Capital: {countryCapital}</h2>
+          <h2>Population: {countryPopulation}</h2>
+          <h2>Area: {countryArea} km²</h2>
+        </div>
       </div>
     </div>
   );
